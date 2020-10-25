1465a50ac6dcd8b4a3b0b35c19c4dfcc918501e3
Nikita Spirin <43099701+NikitaSpirin321234@users.noreply.github.com>
2020-10-25 16:14:26 +0300

    Add files via upload

---
+++ Web(5 sem)/js/records.js
@@ -0,0 +1,44 @@
+const table = document.getElementById('records');
+
+let records = JSON.parse(localStorage["tetris.records"])['records'];
+
+console.log("Records: ", records);
+
+let lastPlayer = records[records.length - 1];
+
+records.sort((a, b) => {
+    return b.score - a.score;
+})
+let i = 0;
+for (let row of records){
+    if (i >= 10) {
+        break;
+    }
+    if(row) {
+        let tr = document.createElement('tr');
+        let name = document.createElement('td');
+        let score = document.createElement('td');
+        name.textContent = row.name;
+        score.textContent = row.score;
+        tr.appendChild(name);
+        tr.appendChild(score);
+        table.appendChild(tr);
+        i++;
+    }
+}
+
+let tr = document.createElement('tr');
+let currentResult = document.createElement('td');
+currentResult.colSpan = 2;
+currentResult.innerHTML = '<b>Ваш результат</b>';
+tr.appendChild(currentResult);
+table.appendChild(tr);
+
+tr = document.createElement('tr');
+let name = document.createElement('td');
+let score = document.createElement('td');
+name.textContent = lastPlayer.name;
+score.textContent = lastPlayer.score;
+tr.appendChild(name);
+tr.appendChild(score);
+table.appendChild(tr);
---
+++ Web(5 sem)/js/view.js
@@ -0,0 +1,114 @@
+class View{
+    constructor(game){
+        this.game = game;
+        let name = localStorage["tetris.username"];
+        let fullName;
+        if(name)
+            fullName = "Игрок: " + localStorage["tetris.username"];
+        else {
+            fullName = "Игрок: Player";
+            localStorage["tetris.username"] = "Player";
+        }
+        document.getElementById("playerName").textContent = fullName;
+    }
+
+    viewBlock(context, code, x, y, width, height){
+        let color = '';
+        switch (code) {
+            case 1:
+                color = 'cyan';
+                break;
+            case 2:
+                color = 'orange';
+                break;
+            case 3:
+                color = 'blue';
+                break;
+            case 4:
+                color = 'red';
+                break;
+            case 5:
+                color = 'green';
+                break;
+            case 6:
+                color = 'yellow';
+                break;
+            case 7:
+                color = 'purple';
+                break;
+            default:
+                break;
+        }
+        if (color !== '') {
+            context.fillStyle = color;
+            context.strokeStyle = "black";
+            context.strokeWidth = 1;
+            context.fillRect(x * width, y * height, width, height);
+            context.strokeRect(x * width, y * height, width, height);
+        }
+
+    }
+
+}
+
+export class ViewGameField extends View{
+    canvas = document.getElementById("glass");
+    context = this.canvas.getContext("2d");
+    widthOfBlock = this.game.field.getBlockWidthPx;
+    heightOfBlock = this.game.field.getBlockHeightPx;
+
+    constructor(game){
+        super(game);
+    }
+
+    viewGF(state){
+        for(let y = 0; y < state.length; y++)
+            for(let x = 0; x < state[y].length; x++) {
+                let code = state[y][x];
+                if (code) {
+                    this.viewBlock(this.context, code, x, y, this.widthOfBlock, this.heightOfBlock);
+                }
+            }
+    }
+
+    viewGameOverScreen(){
+        this.context.strokeStyle = "#ff3121";
+        this.context.shadowColor = "#ff5445";
+        this.context.shadowOffsetX = 1;
+        this.context.shadowOffsetY = 1;
+        this.context.font = "40pt Calibri";
+        this.context.strokeText("Game over!", 25, 200);
+        this.context.font = "22pt Calibri";
+        this.context.strokeText("Your total score is " + this.game.score, 25, 250);
+    }
+
+    clearScreen(){
+        this.context.clearRect(0, 0, this.game.field.getWidthPx, this.game.field.getHeightPx);
+    }
+}
+
+export class ViewNextBrick extends View{
+    canvas = document.getElementById("nextBrick");
+    context = this.canvas.getContext("2d");
+    widthOfBlock = this.game.field.getBlockWidthPx;
+    heightOfBlock = this.game.field.getBlockHeightPx;
+
+    constructor(game){
+        super(game);
+    }
+
+    viewB(){
+        let nextFigure = this.game.nextFigure;
+        for(let y = 0; y < nextFigure.figure.length; y++)
+            for(let x = 0; x < nextFigure.figure.length; x++) {
+                let code = nextFigure.figure[y][x];
+                if (code)
+                    this.viewBlock(this.context, code, x, y, this.widthOfBlock, this.heightOfBlock);
+            }
+    }
+
+    clearBrick(){
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+}
